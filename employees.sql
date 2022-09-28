39d0aa7a509915111443e589239b04ad58f1f26e
Mahad444 <mahadsaid95@icloud.com>
2022-09-29 00:12:48 +0300

    My today's table

---
+++ employees.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 28, 2022 at 11:10 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `exercise1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `staff_no` int(11) NOT NULL,
+  `First_Name` varchar(20) NOT NULL,
+  `Last_Name` varchar(20) NOT NULL,
+  `Gender` varchar(1) NOT NULL,
+  `Position` varchar(20) NOT NULL,
+  `Salary` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`staff_no`, `First_Name`, `Last_Name`, `Gender`, `Position`, `Salary`) VALUES
+(1001, 'John', 'Kamau', 'M', 'Clerk', 50000),
+(1002, 'Ibrahim', 'Kamau', 'M', 'Supervisor', 70000),
+(1003, 'Ahmed', 'Bahaj', 'M', 'Manager', 80000),
+(1004, 'Mrembo', 'Didan', 'F', 'Receptionist', 30000),
+(1005, 'Mary', 'Mwangi', 'F', 'Chef', 25000),
+(1006, 'Mahad', 'Said', 'M', 'Senior Staff', 100000),
+(1007, 'Aisha', 'Farah', 'F', 'Trainer', 55000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`staff_no`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `staff_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1008;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
