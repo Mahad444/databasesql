7c8c941af95a8dfe8c932c1424067663f05725ba
Mahad444 <mahadsaid95@icloud.com>
2022-10-06 01:32:50 +0300

    learning destructuring in js

--- NodeApp/nodejs/node.js
+++
@@ -1,8 +0,0 @@
-const hey=12;
-if (90 > hey )
-{
- console.log('Greater number')  
-}
-else{
-    console.log('Lesser Number')
-}
---
+++ data.js
@@ -0,0 +1,44 @@
+var data = [
+    {
+        id:1,
+        Image:IMG1,
+        title:"Covid-19 Tracker",
+        github:"https://github.com",
+        demo:"https://instagram.com"
+    },
+    {
+        id:2,
+        Image:IMG2,
+        title:"Business Analysis",
+        github:"https://github.com",
+        demo:"https://instagram.com"
+    },
+    {
+        id:3,
+        Image:IMG3,
+        title:"Embedded Programming",
+        github:"https://github.com",
+        demo:"https://instagram.com"
+    },
+    {
+        id:4,
+        Image:IMG4,
+        title:"Programming",
+        github:"https://github.com",
+        demo:"https://instagram.com"
+    },
+    {
+        id:5,
+        Image:IMG5,
+        title:"Bakery Website",
+        github:"https://github.com",
+        demo:"https://instagram.com"
+    },
+    {
+        id:6,
+        Image:IMG1,
+        title:"Forex",
+        github:"https://github.com",
+        demo:"https://instagram.com"
+    }
+  ];
